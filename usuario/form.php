380e5dde52a188e9c4714f83d367b67c9fba4737
marcos <marcosbmx120@hotmail.com>
2019-02-07 16:27:55 -0200

    feito update no arquivo form da pasta usuario

--- usuario/form.php
+++ usuario/form.php
@@ -5,7 +5,8 @@ $email = $_POST['email'];
 $senha = $_POST['senha'];
 require_once "../db/Usuario.php";
 if($id > 0){
-	
+	$sql = "UPDATE usuario SET id = '$id'";
+	$rs = mysqli_query(Conn.php::conectar());
 }else{
 	$usuario = Usuario::inserir($nomeusuario, $email, $senha);
 }
